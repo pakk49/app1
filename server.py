4af12cc22efafa1e6483821b12ff6eecc1e0770f
Your Name <you@example.com>
2025-05-14 22:12:17 +0700

    FIX ERROR-500

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
+import os
+import sys
+
+# เพิ่ม path ของโฟลเดอร์ src
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 from waitress import serve
 from app import app
+import os
 
 if __name__ == '__main__':
-    print("เริ่มต้นเซิร์ฟเวอร์ที่ http://localhost:8080")
-    serve(app, host='127.0.0.1', port=8080, threads=6)
+    port = int(os.environ.get("PORT", 8080))
+    print(f"เริ่มต้นเซิร์ฟเวอร์ที่ http://localhost:{port}")
+    serve(app, host='0.0.0.0', port=port)
