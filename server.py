3aa584d45c9ed403c8abbe6608725efadae5cfb8
Your Name <you@example.com>
2025-05-14 21:51:14 +0700

    FIX

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == '__main__':
     print("เริ่มต้นเซิร์ฟเวอร์ที่ http://localhost:8080")
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host='127.0.0.1', port=8080, threads=6)
