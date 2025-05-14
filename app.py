6ec66c808116a2878d79679cd1cf67c68c6aa3d3
Your Name <you@example.com>
2025-05-15 01:06:27 +0700

    fix

--- app.py
+++ app.py
@@ -241,5 +241,4 @@ def from_json(value):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- server.py
+++ server.py
@@ -1,4 +1,10 @@
+import os
+import sys
 from waitress import serve
+
+# เพิ่ม path ของโปรเจค
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app import app
 
 if __name__ == '__main__':
